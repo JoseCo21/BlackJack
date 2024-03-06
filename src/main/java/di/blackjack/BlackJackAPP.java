abbad6294a83ccc1bad1ad7b4493d9a194a8a5b3
jose <jose.manuel.cardoso@iessanmamede.com>
2024-03-06 15:18:01 +0100

    Inicio proyecto

---
+++ src/main/java/di/blackjack/BlackJackAPP.java
@@ -0,0 +1,23 @@
+package di.blackjack;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class BlackJackAPP extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(BlackJackAPP.class.getResource("blackJackVista.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("BlackJack");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/di/blackjack/BlackJackControlador.java
@@ -0,0 +1,14 @@
+package di.blackjack;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class BlackJackControlador {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
