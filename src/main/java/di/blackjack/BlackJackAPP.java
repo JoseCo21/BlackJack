945dd065053d1c909c461143448c7b3bf35501d7
foncu <jose.manuel.cardoso@iessanmamede.com>
2024-03-10 16:41:00 +0100

    Commit 6 Modificaciones varias

--- src/main/java/di/blackjack/BlackJackAPP.java
+++ src/main/java/di/blackjack/BlackJackAPP.java
@@ -11,7 +11,7 @@ public class BlackJackAPP extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(BlackJackAPP.class.getResource("blackJackVista.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1900, 1060);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         stage.setTitle("BlackJack");
         stage.setScene(scene);
         stage.show();
